058a4d280beb9a98f2aa3afd12bc54e6873e3107
CRW <chris.wilhite@datastax.com>
2020-11-24 15:11:59 -0500

    step 2 solution

--- functions/helloWorld.js
+++ functions/helloWorld.js
@@ -1,3 +1,7 @@
 exports.handler = async (event, context) => {
   // let's return a JSON response that looks like: { hello: "world" }
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ hello: "world" }),
+  };
 };
