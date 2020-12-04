a92bcabc009452697169acead97976a2ee70c507
Rebecca Mills <beccamills86@gmail.com>
2020-12-04 11:55:03 -0330

    Merging step1 into master

--- functions/helloWorld.js
+++ functions/helloWorld.js
@@ -1,3 +1,6 @@
 exports.handler = async (event, context) => {
-  // let's return a JSON response that looks like: { hello: "world" }
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ hello: "world" }),
+  };
 };
